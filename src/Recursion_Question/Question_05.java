c87d5d1f48a05c139ec951cc5d0649705018c2c0
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-04-12 17:37:07 +0530

    Question_05

--- src/Recursion_Question/Question_05.java
+++ src/Recursion_Question/Question_05.java
@@ -2,17 +2,14 @@ package Recursion_Question;
 
 public class Question_05 {
     public static void main(String[] args) {
-        int n=9876;
-        System.out.println(Count_digits(n));
-
+    int n=123;
+        System.out.println(SumOfDigit(n));
     }
-    public static int Count_digits(int n){
-        if (n/10==0){
-            return 1;
+    public static int SumOfDigit(int n){
+        if (n%10==n){
+            return n;
         }
-
-        int C=Count_digits(n/10);
-        return C+1;
-
+        int sum=SumOfDigit(n/10);
+        return sum + n % 10;
     }
 }
