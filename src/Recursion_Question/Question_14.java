1946638c3d350f9a026de79a9294d123ba291a03
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-04-12 22:05:04 +0530

    Check Array Is Sorted

---
+++ src/Recursion_Question/Question_14.java
@@ -0,0 +1,17 @@
+package Recursion_Question;
+
+public class Question_14 {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,2};
+        System.out.println(IsSorted(arr,0));
+    }
+    public static boolean IsSorted(int[]arr,int i){
+        if (i== arr.length-1){
+            return true;
+        }
+        if (arr[i]>arr[i+1]){
+            return false;
+        }
+        return IsSorted(arr,i+1);
+    }
+}
