602a3be9f32d039162b2a2d01043ceb232f883e3
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-04-12 16:32:48 +0530

    Question_01

--- src/Recursion_Question/Question_01.java
+++ src/Recursion_Question/Question_01.java
@@ -1,4 +1,16 @@
 package Recursion_Question;
 
 public class Question_01 {
+    public static void main(String[] args) {
+        int n=5;
+        Print(n);
+    }
+    public static void Print(int n){
+        if (n==0){
+            return;
+        }
+
+        Print(n-1);
+        System.out.println(n);
+    }
 }
