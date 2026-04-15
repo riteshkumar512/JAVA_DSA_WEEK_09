10fd8c47ad1f67e9feb9dd5bbb6d1032a6f6270e
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-04-15 11:42:31 +0530

    Fix UpperCase too

--- src/Recursion_Question/Question_20.java
+++ src/Recursion_Question/Question_20.java
@@ -2,7 +2,7 @@ package Recursion_Question;
 
 public class Question_20 {
     public static void main(String[] args) {
-        String s="coding";
+        String s="HELLO";
         System.out.println(CountVowel(s));
 
     }
@@ -12,7 +12,7 @@ public class Question_20 {
         }
         char ch=s.charAt(0);
         int c=0;
-        if (ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u'){
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O'|| ch=='U'){
             c++;
         }
         return c + CountVowel(s.substring(1));
